79d7aa9c97a5e2c6c5c12abe316b427b67bf4c99
Vincent <vleclerc@inow.fr>
2018-06-14 10:38:47 +0200

    add model task and migrations for task and identity

--- NetSizeAPI/Data/NetSizeDbContext.cs
+++ NetSizeAPI/Data/NetSizeDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NetSizeAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,9 @@ namespace NetSizeAPI.Data
     {
         public NetSizeDbContext(DbContextOptions<NetSizeDbContext> options) : base(options)
         {
-                
+         
         }
+
+        public DbSet<AppTask> Tasks { get; set; }
     }
 }
---
+++ NetSizeAPI/Models/AppTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetSizeAPI.Models
+{
+    public class AppTask
+    {
+        public int ID { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
